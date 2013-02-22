effb80299c3a48e54889d7ee63d457157f659a0c
CARLOS SILVA <cefs1984@gmail.com>
2013-02-22 03:34:53 -0300

    Preventing eager loading

--- app/models/question_filter.rb
+++ app/models/question_filter.rb
@@ -2,7 +2,7 @@ class QuestionFilter
    UNANSWERED = "unanswered"
 
    def self.filter(filter)
-      scope = Question.recent
+      scope = Question.recent.includes(:user, :category)
       scope = scope.unanswered if filter == UNANSWERED      
       scope
    end
